7b0f03a47f46747524d1d15a1bb80f6c8add53e1
JiwonKim <jiwonmik@gmail.com>
2020-10-08 22:49:03 +0900

    change URL Parameter

--- app/src/main/java/com/example/myapplication/GetFireStation.kt
+++ app/src/main/java/com/example/myapplication/GetFireStation.kt
@@ -36,29 +36,13 @@ class GetFireStation : AppCompatActivity() {
         val urlBuilder =
             StringBuilder("http://openapi.safekorea.go.kr/openapi/service/firestation/item") /*URL*/
         urlBuilder.append(
-            "?" + URLEncoder.encode("serviceKey", "UTF-8")
-                .toString() + URLEncoder.encode("=3dItxsHHzQJiyIitvKVQUr8%2ByHe5Qoov%2F5P2PbQ5IHpGhY5DBOO5J05edGISs%2BpNk1vMZzGX4K5XD1VvLBIIjw%3D%3D", "UTF-8")
+            "?" + URLEncoder.encode("ServiceKey", "UTF-8")
+                .toString() + "=" + "3dItxsHHzQJiyIitvKVQUr8%2ByHe5Qoov%2F5P2PbQ5IHpGhY5DBOO5J05edGISs%2BpNk1vMZzGX4K5XD1VvLBIIjw%3D%3D"
         ) /*Service Key*/
         urlBuilder.append(
-            "&" + URLEncoder.encode("firestation_addr_cd", "UTF-8")
-                .toString() + "=" + URLEncoder.encode("3011000000", "UTF-8")
+                "&" + URLEncoder.encode("firestation_addr_cd", "UTF-8")
+                    .toString() + "=" + URLEncoder.encode("3011000000", "UTF-8")
         ) /*법정동코드*/
-        urlBuilder.append(
-            "&" + URLEncoder.encode("numOfRows", "UTF-8")
-                .toString() + "=" + URLEncoder.encode("10","UTF-8")
-        )/* URL의 하나의 페이지에서 볼 수 있는 데이터의 개수 */
-        urlBuilder.append(
-            "&" + URLEncoder.encode("pageNo", "UTF-8")
-                .toString() + "=" + URLEncoder.encode("1","UTF-8")
-        )/* URL 페이지 번호*/
-        urlBuilder.append(
-            "&" + URLEncoder.encode("MobileOS", "UTF-8")
-                .toString() + "=" + URLEncoder.encode("ETC","UTF-8")
-        )/* 사용할 OS 구분 */
-        urlBuilder.append(
-            "&" + URLEncoder.encode("MobileApp", "UTF-8")
-                .toString() + "=" + URLEncoder.encode("SOS","UTF-8")
-        )/* 사용할 어플명, 활용통계 산출하기 위한 항목 */
 
         val url = URL(urlBuilder.toString())
         val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
@@ -79,5 +63,6 @@ class GetFireStation : AppCompatActivity() {
         rd.close()
         conn.disconnect()
         println("성공"+sb.toString())
+        println(url)
     }
 }
