3b3db32ffc3abbb2ace873b39690f8aec92a3ec7
gimhamin033 <gimhamin033@gmail.com>
2020-11-25 18:40:48 +0900

    activity_rescue_login.xml, RescueLoginActivity.kt

--- app/src/main/java/com/example/myapplication/RescueLoginActivity.kt
+++ app/src/main/java/com/example/myapplication/RescueLoginActivity.kt
@@ -9,9 +9,12 @@ import android.os.Bundle
 import android.os.StrictMode
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_my_location.*
 import kotlinx.android.synthetic.main.activity_rescue_login.*
@@ -26,36 +29,38 @@ class RescueLoginActivity : Activity(){
     lateinit var adapter: ArrayAdapter<String>
     var list = ArrayList<String>()
 
+    // 검색어를 입력할 Input 창
+    private var editSearch: EditText? = null
 
+    private var arraylist: java.util.ArrayList<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rescue_login)
 
+        list = java.util.ArrayList()
+
+        arraylist = java.util.ArrayList()
+
+        arraylist!!.addAll(list as java.util.ArrayList<String>)
+
+        //검색하는 창
+        editSearch = findViewById<View>(R.id.editSearch) as EditText
+
+        //리스트 형식으로 소방서목록 받아온것
         fire_listview.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
 
-//        editTextFilter.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                val filterText = edit.toString()
-//                if (filterText.isNotEmpty()) {
-//                    fire_listview.setFilterText(filterText)
-//                } else {
-//                    fire_listview.clearTextFilter()
-//                }
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//            }
-//        })
-
-//        fire_listview.setOnClickListener {
-//            val intent = Intent(this, RescueNumberActivity::class.java)
-//            startActivity(intent)
-//        }
+        // input창에 검색어를 입력시 "addTextChangedListener" 이벤트 리스너를 정의한다.
+        editSearch!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+            override fun afterTextChanged(editable: Editable) {
+                // input창에 문자를 입력할때마다 호출된다.
+                // search 메소드를 호출한다.
+                val text = editSearch!!.text.toString()
+                search(text)
+            }
+        })
 
         StrictMode.enableDefaults()
 
@@ -113,6 +118,27 @@ class RescueLoginActivity : Activity(){
 
     }
 
+    fun search(charText: String) {
+
+        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
+        list!!.clear()
+
+        // 문자 입력이 없을때는 모든 데이터를 보여준다.
+        if (charText.length == 0) {
+            list!!.addAll(arraylist!!)
+        } else {
+            // 리스트의 모든 데이터를 검색한다.
+            for (i in arraylist!!.indices) {
+                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
+                if (arraylist!![i].toLowerCase().contains(charText)) {
+                    // 검색된 데이터를 리스트에 추가한다.
+                    list!!.add(arraylist!![i])
+                }
+            }
+        }
+        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
+        adapter?.notifyDataSetChanged()
+    }
 
 
     //리스트 다이얼로그 생성
